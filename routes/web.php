1e889a73ecbfc6ba90a28b5f6b503c0e0222dee6
nasir <ali.nasir.0104@gmail.com>
2022-08-07 18:45:26 +0430

    generate base project and just playing with some directories and files

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Task;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tasks', function() {
+    $tasks = Task::all()->where('id', 5);
+
+    dd($tasks);
+});
